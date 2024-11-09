99c2484aab25f00289d6d9698aaeab1cdf59cec7
Pappu <146848823+Pappu-Naskar@users.noreply.github.com>
2024-11-09 02:11:15 +0000

    js code

---
+++ js_basics/dataTypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive dataType
+
+// 7 types : String, Number, Boolearn, Null, undefined, Symbol, BigInt
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+// const bigNumber = 51689655154254;
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "pappu",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("hello world");
+};
+
+// learn  more type of operator results : https://262.ecma-international.org/5.1/index.html#sec-11.4.3
+
+//********************************************************************************************************************** */
+
+// Stack(Primitive), Heap(Non-Primitive)
