afda01029b0df85705ca2365f20aa88704574649
Pappu <146848823+Pappu-Naskar@users.noreply.github.com>
2024-11-18 10:10:55 +0530

    js

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser😑
+
+//code readability should  be high
+
+let name = "pappu";
+let age = 21;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefine =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,43 @@
+// let age = "pappuc";
+
+// console.log(typeof age);
+// console.log(typeof age);
+
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "44sa" => NaN
+// true => 1 ; false => 0
+
+//!IN BOOLEAN CONVERSION OPRATION
+// 1 => true; 0=> false
+// "" => false
+// "pappu" => true
+
+//?**************************************Operations***************************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// let str1 = "hello";
+// let str2 = "User";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3 + 4) * 5) % 3);
+
+let num1, num2, num3;
+
+mum1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
