b65f976917b50267aef3c41175121ec056648000
Pappu <146848823+Pappu-Naskar@users.noreply.github.com>
2024-11-25 02:32:46 +0000

    js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+//? Switch case
+
+const month = 4;
+
+switch (month) {
+  case 1:
+    console.log("jan");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("may");
+    break;
+
+  default:
+    console.log("worng input");
+    break;
+}
